c31802208f8f3549c20efb959ebb0f117a3937a5
Tiago Azevedo <1136009+tabazevedo@users.noreply.github.com>
2024-05-01 17:37:14 +0100

    Remove swc/register and swcrc as it interferes with external transpilers

---
+++ test/register.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-commonjs */
+const { transformSync } = require('@swc/core')
+const pirates = require('pirates')
+const fs = require('fs')
+const path = require('path')
+const swcrc = JSON.parse(fs.readFileSync(path.join(__dirname, '../.swc-cjs')))
+
+pirates.addHook(
+  code => {
+    const result = transformSync(code, swcrc)
+    return result.code
+  },
+  { exts: ['.js'], ignoreNodeModules: false },
+)
